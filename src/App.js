fd78b268e5458a26b01bd37dbd8f0cf01ad05dac
Stephen Wayne <stephen@jstar.ai>
2018-10-15 21:00:02 -0600

    Grab contacts, display on web

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
 import LightList from "./components/LightList";
 
-const lights = [
-  { id: 1, name: "Dimmable light" },
-  { id: 2, name: "Color light" },
-  { id: 3, name: "Temperature light" },
-];
-
 class App extends Component {
+
+  // default State object
+  state = {
+    contacts: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        // create an array of contacts only with relevant data
+        const newContacts = response.data.map(c => {
+          return {
+            id: c.id,
+            name: c.name
+          };
+        });
+
+        // create a new "State" object without mutating original State object
+        const newState = Object.assign({}, this.state, {
+          contacts: newContacts
+        });
+
+        // store the new state object in the component's state
+        this.setState(newState);
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,7 +43,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to the Hue API consumer</h1>
         </header>
 
-        <LightList lights={lights} />
+        <LightList lights={this.state.contacts} />
       </div>
     );
   }
